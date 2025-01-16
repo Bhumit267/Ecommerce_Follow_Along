cd7ed2efe0606e2a03b7da0ae9b5fa2bc348c2ea
Bhumit267 <bhumitprs15101975@gmail.com>
2025-01-16 13:55:10 +0530

    made signupPage

--- frontend/front-end/src/App.jsx
+++ frontend/front-end/src/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import './App.css';
 import { LoginPage } from "./Routes/Routes.js";
+import { SignupPage } from "./Routes/Routes.js"
 
 
 
-const App = () => {
+const App = () => { 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage/>} />
+
       </Routes>
     </BrowserRouter>
   )
