9782f33096828ea80874162ad3c280624b3c4843
Bhumit267 <bhumitprs15101975@gmail.com>
2025-01-24 13:13:07 +0530

    milestone-8

--- frontend/front-end/src/App.jsx
+++ frontend/front-end/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import './App.css';
-import { LoginPage } from "./Routes/Routes.js";
+import { LoginPage, Home } from "./Routes/Routes.js";
 import { SignupPage } from "./Routes/Routes.js"
 
 
@@ -10,6 +10,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage/>} />
 
