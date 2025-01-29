854ddd78444678943aca512a86db21b8a5852e0c
Bhumit267 <bhumitprs15101975@gmail.com>
2025-01-29 17:37:09 +0530

    adding backend for createproduct

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,16 +1,29 @@
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },   
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round.apply(Math.random() * 1e9);
-        //Define a unique name for the file
+    destination: '../uploads',
+    filename: function (req, file, cb) {
+        console.log(req.body);
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1000);
         const filename = file.originalname.split('.')[0];
-        cb(null, filename + "-"+ uniqueSuffix + ".png");
-
-    },
+        cb(null, filename + "-" + uniqueSuffix + ".png");
+    }
 });
 
-exports.upload = multer({storage: storage});
+const pstorage = multer.diskStorage({
+    destination: '../products',
+    filename: function (req, file, cb) {
+        console.log(req.body);
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1000);
+        const filename = file.originalname.split('.')[0];
+        cb(null, filename + "-" + uniqueSuffix + ".png");
+    }
+});
+
+
+
+
+
+
+exports.upload = multer({storage:storage});
+exports.pupload = multer({storage:pstorage});
